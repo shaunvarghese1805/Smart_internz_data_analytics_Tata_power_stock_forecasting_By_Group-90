1f01f1bc49437d43bed0e3c8b4b622bbdb10fdf3
Shaun Jacob Varghese <shaun.varghese1805@gmail.com>
2023-06-30 00:36:22 +0530

    Delete

--- app_run.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template,url_for,redirect
-app = Flask(__name__)
-
-@app.route('/')
-def main():
-   return render_template('index.html')
-   #main driver function
-if __name__ == '__main__':
-   app.run(debug=False,port=8000)
